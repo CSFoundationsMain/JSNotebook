146ae5ea64de1f7300941250173ec2c5fd0cc255
EC2 Default User <ec2-user@ip-172-31-55-171.ec2.internal>
2022-11-23 21:12:32 +0000

    Begin JS Notebook

---
+++ intro.js
@@ -0,0 +1,20 @@
+/*
+JavaScript (JS) is a programming language that is one of the core technologies of the World Wide Web,
+alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior,
+often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to
+execute the code on users' devices.
+
+JavaScript engines were originally used only in web browsers, but are now core components of some servers
+and a variety of applications. The most popular runtime system for this usage is Node.js.
+
+         - https://en.wikipedia.org/wiki/JavaScript
+         
+        HTML -          nouns
+        CSS -           adjectives
+        JavaScript -    verbs
+*/
+
+
+/*  The Document Object Model (DOM)  */
+
+const myFirstVariable = document.getElementById("intro");
---
+++ variables.js
@@ -0,0 +1,26 @@
+/* When to use variables:  W3Schools
+Always declare JavaScript variables with var,let, orconst.
+
+The var keyword is used in all JavaScript code from 1995 to 2015.
+
+If you want a general rule: always declare variables with const.
+
+If you think the value of the variable can change, use let.
+*/
+
+// Declare variables 
+let score = 0;
+let lives = 0;
+
+const intro = document.getElementById("intro");
+const variables = document.getElementById("variables");
+
+
+/* Primitive Data Types (most commonly used)
+   the basics building blocks */
+    let myNumber = 7;           // Number - JS has only one number type
+    let myName = "Michael";     // String - series of characters - must be in ""
+    let isRetired = false;      // Boolean
+    Null
+    Undefined
+    
