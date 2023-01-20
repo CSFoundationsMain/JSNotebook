000cd107da57af089b27d40cfa2e630fff045e0e
Michael Cullen <mcullen@region16ct.org>
2023-01-20 09:41:38 -0500

    Update intro.js

--- intro.js
+++ intro.js
@@ -23,8 +23,8 @@ ECMA-262 is the official name of the standard. ECMAScript is the official name o
 
 /* Primitive Data Types (most commonly used)
    the basics building blocks */
-    let myNumber = 7;           // Number - JS has only one number type
-    let myName = "Michael";     // String - series of characters - must be in ""
-    let isRetired = false;      // Boolean - true or false
-    let level;                  // Undefined
-    Null
+                                                                        // Number - JS has only one number type
+                                                                        // String - series of characters - must be in ""
+                                                                        // Boolean - true or false
+                                                                        // Undefined
+    
